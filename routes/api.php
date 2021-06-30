a32f501225a80e7cf54aff82c5e5b1bf7fbf30cd
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 12:50:28 -0300

    add api route adress

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TelephoneController;
+use App\Http\Controllers\AdressController;
 
 Route::apiResource('user', UserController::class);
 
 Route::apiResource('telephone', TelephoneController::class);
+
+Route::apiResource('adress', AdressController::class);
