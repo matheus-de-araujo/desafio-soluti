cfaf2a548dfa7fec64d4f6780c4f31c355668eaf
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 10:36:02 -0300

    alter name telephone to Telephone

--- app/Models/Telephone.php
+++ app/Models/Telephone.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class telephone extends Model
+class Telephone extends Model
 {
     use HasFactory;
 
