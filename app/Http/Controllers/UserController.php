0ad6f478b59df35db42e40c07e25b2eb579b06db
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 09:30:08 -0300

    update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,78 +7,44 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Lista todos os usuários
      */
     public function index()
     {
-        //
+        return User::all();
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * Salva os Usuários de acordo com os dados do $request
      */
     public function store(Request $request)
     {
-        //
+        User::create($request->all());
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Lista usuário de acordo com o $id
      */
     public function show($id)
     {
-        //
+        return User::findOrFail($id);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Atualiza informações do usuário com o $id de acordo com o $request
      */
     public function update(Request $request, $id)
     {
-        //
+        $user =  User::findOrFail($id);
+        $user->update($request->all());
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Exclui o usuário com o $id
      */
     public function destroy($id)
     {
-        //
+        $user =  User::findOrFail($id);
+        $user->delete();
     }
 }
