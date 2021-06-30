83c21e8e8f472cb2f6dd850e7672949224457322
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 10:39:32 -0300

    add resouce on TelephoneController

--- app/Http/Controllers/TelephoneController.php
+++ app/Http/Controllers/TelephoneController.php
@@ -7,78 +7,44 @@ use Illuminate\Http\Request;
 class TelephoneController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Lista todos os Telefones
      */
     public function index()
     {
-        //
+        return Telephone::all();
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * Salva os telefones de acordo com os dados do $request
      */
     public function store(Request $request)
     {
-        //
+        Telephone::create($request->all());
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Lista o telefone de acordo com o $id
      */
     public function show($id)
     {
-        //
+        return Telephone::findOrFail($id);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Atualiza informações do telefone com o $id de acordo com o $request
      */
     public function update(Request $request, $id)
     {
-        //
+        $telephone =  Telephone::findOrFail($id);
+        $telephone->update($request->all());
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Exclui o telefone com o $id
      */
     public function destroy($id)
     {
-        //
+        $telephone =  Telephone::findOrFail($id);
+        $telephone->delete();
     }
 }
