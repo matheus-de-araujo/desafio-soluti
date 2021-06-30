9db3ea89cb024bf879b47ed0046b67759adcedd8
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 10:31:59 -0300

    add relation with user

---
+++ database/migrations/2021_06_30_131607_create_telephones_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelephonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('telephones', function (Blueprint $table) {
+            $table->id();
+            $table->string('telephone');
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telephones');
+
+        $table->dropForeign('telephones_user_id_foreign');
+    }
+}
