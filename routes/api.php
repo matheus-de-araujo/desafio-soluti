acb74fe4e92a0945b8577f33b1b26827c12b48ee
Matheus de Araujo <matheus.encode@gmail.com>
2021-06-30 10:32:57 -0300

    add api route to telephone

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TelephoneController;
 
 Route::apiResource('user', UserController::class);
+
+Route::apiResource('telephone', TelephoneController::class);
