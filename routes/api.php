584ae94e4acf580320fa7353be2856283ab45fd2
Matheus de Araujo <matheus.encode@gmail.com>
2021-07-01 14:14:49 -0300

    update routes api

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,15 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CertificateController;
 use App\Http\Controllers\AuthenticateController;
 
-Route::apiResource('telephone', TelephoneController::class);
-
-Route::apiResource('address', AddressController::class);
-
 Route::apiResource('certificate', CertificateController::class);
 
-Route::post('login', [AuthenticateController::class, 'login'])->name('login');
+Route::get('login', [AuthenticateController::class, 'login'])->name('login');
+
+Route::post('user', [UserController::class, 'store']);
 
 Route::middleware('auth:api')->group( function () {
   
-  Route::apiResource('user', UserController::class);
+  Route::apiResource('user', UserController::class)->except(['store']);
   
   Route::post('logout', [AuthenticateController::class, 'logout'])->name('logout');
 
